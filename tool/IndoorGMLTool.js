92da59145de34413688148d7aa4fdf52d49a7000
park singyu <psk876@naver.com>
2020-04-16 14:35:21 +0900

    getIndoorGMLVectorArray has been changed xmltojs

--- tool/IndoorGMLTool.js
+++ tool/IndoorGMLTool.js
@@ -73,100 +73,42 @@ export default class {
         })
     }
 
-    getIndoorGMLVectorArray (file){
+    getIndoorGMLVectorArray(file){
         return new Promise((resolve,reject)=>{
             var reader = new FileReader();
             reader.readAsText(file,"euc-kr");
-
             reader.onload = () => {
                 this.readed_indoorGML = reader.result;
-                this.primalSpaceFeatures = this.extractToTag(this.readed_indoorGML,'<core:PrimalSpaceFeatures');
-                this.cellSpaceMember_array = this.primalSpaceFeatures.split('<core:cellSpaceMember>');
-                this.cellSpaceMember_array.map(cellspaceMember => {
-                    this.linearRing_array = this.linearRing_array.concat(this.extractToTag(cellspaceMember.split('<gml:Polygon>')[1],'<gml:LinearRing>'));
-                })
 
-                this.linearRing_array.map((linearRing,index)=>{
-                    if (index > 0) {
+                var parser = new xml2js.Parser();
+
+                parser.parseStringPromise(this.readed_indoorGML).then((result)=>{
+                    let PrimalSpaceFeatures = result['core:IndoorFeatures']['core:primalSpaceFeatures'][0]['core:PrimalSpaceFeatures'][0];
+                    let cellSpaceMemberList = PrimalSpaceFeatures['core:cellSpaceMember'];
+                    
+                    cellSpaceMemberList.map((cellSpaceMember, index)=>{
+                        let surfaceMemberList = cellSpaceMember['core:CellSpace'][0]['core:cellSpaceGeometry'][0]['core:Geometry3D'][0]['gml:Solid'][0]['gml:exterior'][0]['gml:Shell'][0]['gml:surfaceMember'];                        
                         let polygon_array = [];
-                        let pos_array = linearRing.split('<gml:pos');
-                        
-                        pos_array.map((pos,index) =>{
-                            if(index > 0) {
-                                pos = this.extractContent(pos);
-                                let polygon = pos.split(' ');
-                                polygon.pop();
+                        surfaceMemberList.map((surfaceMember,surfaceMemberIndex)=>{
+                            if (surfaceMemberIndex == 0){
+                                let posList = surfaceMember['gml:Polygon'][0]['gml:exterior'][0]['gml:LinearRing'][0]['gml:pos'];
+                                posList.map(pos=>{
+                                    let polygon = pos['_'].split(' ');
+                                    polygon.pop();
     
-                                polygon[0] = parseFloat(polygon[0]);
-                                polygon[1] = parseFloat(polygon[1]);
-                                
-                                polygon_array.push(polygon);
+                                    polygon[0] = parseFloat(polygon[0]);
+                                    polygon[1] = parseFloat(polygon[1]);
+                                    
+                                    polygon_array.push(polygon);
+                                })
                             }
                         })
-    
                         this.polygons_array.push(polygon_array);
-                    }
+                    })
                 })
                 resolve(this.polygons_array);
             }
-        });
-        
-
-        reader.onload = () => {
-            this.readed_indoorGML = reader.result;
-            //console.log(this.readed_indoorGML);
-            this.primalSpaceFeatures = this.extractToTag(this.readed_indoorGML,'<core:PrimalSpaceFeatures');
-            // this.primalSpaceFeatures.split('<core:cellSpaceMember>').forEach((element,index) => {
-            //     if (index == 0) continue;
-            //     console.log(element);
-            // });
-            // this.cellSpaceMember_array = this.primalSpaceFeatures.split('<core:cellSpaceMember>');
-            // this.cellSpaceMember_array.map
-
-            // this.primalSpaceFeatures.split('<core:cellSpaceMember>').map((cellSpaceMember,index)=>{
-            //     if (index != 0) this.cellSpaceMember_array = this.cellSpaceMember_array.concat(cellSpaceMember);
-            // })
-
-            this.cellSpaceMember_array = this.primalSpaceFeatures.split('<core:cellSpaceMember>');
-
-            this.cellSpaceMember_array.map(cellspaceMember => {
-                //console.log(cellspaceMember.split('<gml:LinearRing>')[1]);
-                this.linearRing_array = this.linearRing_array.concat(this.extractToTag(cellspaceMember.split('<gml:Polygon>')[1],'<gml:LinearRing>'));
-                //console.log(this.extractToTag(cellspaceMember.split('<gml:Polygon>')[1],'<gml:LinearRing>'));
-            })
-
-            //console.log(this.linearRing_array[15]);
-
-            //console.log(this.extractContent(this.extractToTag(this.linearRing_array[15],'<gml:pos')));
-
-            //console.log(this.extractToTag(this.linearRing_array[15],'<gml:pos'));
-
-            //console.log(this.linearRing_array[15].split('<gml:pos srsDimension="3">')[0]);
-            //console.log(this.linearRing_array[15].split('<gml:pos srsDimension="3">')[8]);
-            
-
-            this.linearRing_array.map((linearRing,index)=>{
-                if (index > 0) {
-                    let polygon_array = [];
-                    let pos_array = linearRing.split('<gml:pos');
-                    
-                    pos_array.map((pos,index) =>{
-                        if(index > 0) {
-                            pos = this.extractContent(pos);
-                            let polygon = pos.split(' ');
-                            polygon.pop();
-
-                            polygon[0] = parseFloat(polygon[0]);
-                            polygon[1] = parseFloat(polygon[1]);
-
-                            polygon_array.push(polygon);
-                        }
-                    })
-
-                    this.polygons_array.push(polygon_array);
-                }
-            })
-        }
+        })
     }
 
     extractToTag (indoorGML_data,tagName) {
